c06d5d26ca9d6a7809a85d99a7fcd6ba40ab3470
Abdullah Macalalad <qafmacalalad@tip.edu.ph>
2026-04-12 00:05:18 +0800

    Corrected mismatched music selections for BGM and adjusted volume

--- constants/audio.ts
+++ constants/audio.ts
@@ -1,4 +1,4 @@
 export const MENU_BGM = require("../assets/audio/menu-theme.mp3");
 export const BATTLE_BGM = require("../assets/audio/battle-theme.mp3");
 
-export const DEFAULT_BGM_VOLUME = 0.35;
+export const DEFAULT_BGM_VOLUME = 0.08;
